84ec6556faa8dff2ab8a386299680a152f247cee
vinisaab <viniciusmsaab@gmail.com>
2021-03-09 14:15:28 -0300

    Adjust Connection

---
+++ Api/Utils/ConnectionString.cs
@@ -0,0 +1,10 @@
+namespace Api.Utils
+{
+    public class ConnectionString
+    {
+        public string connectionString(){
+            return "mongodb+srv://root:GgJkLGgfG3l3sPjX@cluster0.jdqwu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+        }
+        
+    }
+}
